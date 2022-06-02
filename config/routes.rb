e54f172b59284bcbe56d05d3d1e7ec7819d2fc64
Lance Liu <lanceliumeng89@gmail.com>
2022-06-02 21:08:18 +1000

    create Random Joke functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   scope "/api" do 
     get "/jokes", to: "jokes#index"
     post "/jokes", to: "jokes#create"
+    get "/jokes/random", to: "jokes#random"
     get "/jokes/:id", to: "jokes#show"
     put "/jokes/:id", to: "jokes#update"
     delete "/jokes/:id", to: "jokes#destory"
-    get "/jokes/random", to: "jokes#random"
   end
 
 end
